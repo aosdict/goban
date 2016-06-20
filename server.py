f92ef798752de348ebcfa78eead6067cec050dee
aosdict <aosdict@gmail.com>
2016-06-20 07:36:16 -0400

    Added enforcing player turn order, selectable port, PASS command, restructuring

--- server.py
+++ server.py
@@ -1,19 +1,26 @@
-# 
 import sys
 import socket
 import thread
 import time
 import goban
 
+if __name__ != '__main__':
+   print 'Error: server.py should only be run as the main module'
+   sys.exit(1)
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 HOST = ''   # Symbolic name, meaning all available interfaces
-PORT = 8888 # Arbitrary non-privileged port
+try:
+   PORT = int(sys.argv[1])
+except:
+   PORT = 8888 # Arbitrary non-privileged port
 
 try:
    s.bind( (HOST, PORT) )
 except socket.error as err:
    print 'Bind failed:', str(err)
+   sys.exit(1)
 
 print 'Bound to port', PORT
 
@@ -21,38 +28,46 @@ print 'Bound to port', PORT
 s.listen(2)
 
 # goban setup
-gb = goban.goban(9)
+gb = goban.Goban(9)
+current_player = 1
 
 def playerthread(conn, player_number):
-   conn.send('sdfsdf\n');
+   global current_player
+   conn.send('Connected. You are player number ' + str(player_number) + '\n');
 
    data = conn.recv(1024)
-   while True:
-      data = conn.recv(1024)
-      if not data:
-         # connection closed
-         break
-
+   while data:
+      # print data
       cmdlist = data.split()
       cmd = cmdlist[0]
       args = cmdlist[1:]
 
       if cmd == 'PLAY':
-         if len(args) != 2:
+         if current_player != player_number:
+            conn.sendall('ERROR: It\'s not your turn\n')
+         elif len(args) != 2:
             conn.sendall('ERROR: Wrong number of arguments for PLAY\n')
          else:
             try:
                row = int(args[0])
                col = int(args[1])
+               gb.play(player_number, row, col)
+               current_player = 2 if (player_number == 1) else 1
+               conn.sendall('SUCCESS: Play made at row '+str(row)+', column '+str(col)+'\n')
+               gb.dbg_print()
+            except goban.GobanError as e:
+               conn.sendall('ERROR: Goban error: '+str(e)+'\n')
             except ValueError:
                conn.sendall('ERROR: row/column argument for PLAY was not an int\n')
-            try:
-               gb.play(player_number, row, col)
-               conn.sendall('SUCCESS: Play made at row '+row+', column '+col+'\n')
-            except Exception as e:
-               conn.sendall('ERROR: Goban error: '+str(e)+'\n')
+               
+      elif cmd == 'PASS':
+         current_player = 2 if (player_number == 1) else 1
+         conn.sendall('SUCCESS: You passed this turn')
       else:
          conn.sendall('ERROR: Unrecognized command '+cmd+'\n')
+      
+      # receive new data
+      data = conn.recv(1024)
 
       # conn.sendall('fdsfds\n')
    print 'Connection 1 closed'
